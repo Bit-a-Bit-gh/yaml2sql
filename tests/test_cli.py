0671560f961594836c81cf3425ef67e63899f5fb
Fernando Rivas <fenandos@hotmail.com>
2020-12-07 22:55:36 -0600

    Add tests to create-tables

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,6 +9,7 @@ This is the test module for the project's command-line interface (CLI)
 module.
 """
 # fmt: off
+from os.path import exists, join, dirname
 import yaml2sql.cli as cli
 from yaml2sql import __version__
 # fmt: on
@@ -17,6 +18,9 @@ from click.testing import CliRunner, Result
 
 # To learn more about testing Click applications, visit the link below.
 # http://click.pocoo.org/5/testing/
+
+
+PATH_EXAMPLE_FILE = join(dirname(__file__), 'example.yaml')
 
 
 def test_version_displays_library_version():
@@ -41,3 +45,25 @@ def test_verbose_output():
     assert (
         "Verbose" in result.output.strip()
     ), "Verbose logging should be indicated in output."
+
+def test_create_tables_stout():
+    """
+    Arrange/Act: Run the `create-tables` command.
+    Assert: Output to stout.
+    """
+    runner: CliRunner = CliRunner()
+    result: Result = runner.invoke(cli.create_tables, [PATH_EXAMPLE_FILE])
+    assert (
+        'CREATE TABLE' in result.output.strip()
+    )
+
+def test_create_tables_file():
+    """
+    Arrange/Act: Run the `create-tables` command.
+    Assert: Output to stout.
+    """
+    runner: CliRunner = CliRunner()
+    result: Result = runner.invoke(cli.create_tables, [PATH_EXAMPLE_FILE, PATH_EXAMPLE_FILE + '.tmp'])
+    assert (
+        exists(PATH_EXAMPLE_FILE + '.tmp')
+    )
--- tests/test_sqls.py
+++ tests/test_sqls.py
@@ -2,10 +2,10 @@
 # -*- coding: utf-8 -*-
 
 """
-.. currentmodule:: test_example
+.. currentmodule:: test_sqls
 .. moduleauthor:: Bit-a-Bit <github@bit-a-bit.info>
 
-This is a sample test module.
+Tests sqls module.
 """
 
 from os.path import exists, dirname, join 
@@ -13,11 +13,8 @@ import pytest
 from yaml2sql.sqls import StatementSQL
 from ruamel.yaml import YAML
 
-"""
-This is just an example test suite.  It will check the current project version
-numbers against the original version numbers and will start failing as soon as
-the current version numbers change.
-"""
+
+PATH_EXAMPLE_FILE = join(dirname(__file__), 'example.yaml')
 
 
 def test_StatementsSQL_init():
@@ -27,27 +24,10 @@ def test_StatementsSQL_init():
     Assert: self.datamodel is not null.
     """
     yaml = YAML(typ='safe')
-    with open(join(dirname(__file__), 'example.yaml')) as f:
+    with open(PATH_EXAMPLE_FILE) as f:
         ssql = StatementSQL(f)
     print(ssql.datamodel)
     assert (
-        not ssql.datamodel is None
-    )
-
-
-@pytest.mark.parametrize("a,b,c", [(1, 2, 5), (3, 4, 25)])
-def test_ab_addSquares_equalsC(a, b, c):
-    """
-    Arrange: Acquire the first two parameters (a and b).
-    Act: Add the squares of the first two parameters (a and b).
-    Assert: The sum of the squares equals the third parameter (c).
-
-    :param a: the first parameter
-    :param b: the second parameter
-    :param c: the result of adding the squares of a and b
-    """
-    assert (
-        a * a + b * b == c,
-        "'c' should be the sum of the squares of 'a' and 'b'. "
-        "This is an example test and can be removed.",
+        not ssql.datamodel is None,
+        not ssql.create_tables is None
     )
